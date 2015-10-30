f51b54684fdcd9df631f338205af62e07caa9ebf
wlandau <will.landau@gmail.com>
2015-10-30 03:06:47 -0500

    mod?

--- R/data.R
+++ R/data.R
@@ -39,7 +39,7 @@ NULL
 make_vignette_data = function(){
   file = "vignette.rda"
   example_generated_data = dat = generate_data(libraries = 12, genes = 20)
-  starting_chain = Chain(dat$counts, dat$design, Configs(diag = "none", ess = 0, max_attempts = 5, iterations = 100, burnin = 100, thin = 0))
+  starting_chain = Chain(dat$counts, dat$design, Configs(diag = "none", ess = 0, iterations = 100, burnin = 100, thin = 0))
   ending_chain = fbseq(starting_chain)
   save(example_generated_data, starting_chain, ending_chain, file = file)
 }
