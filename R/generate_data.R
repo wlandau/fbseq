ef2706993e186d809b049b076031093c2c9423c7
wlandau <will.landau@gmail.com>
2015-10-31 09:52:06 -0700

    starts and data

--- R/generate_data.R
+++ R/generate_data.R
@@ -18,7 +18,7 @@
 #' Can be among "Laplace", "t", or "horseshoe". All other values will default to the normal prior.
 generate_data = function(libraries = 12, genes = 3.5e4, 
           design = cbind(rep(1, libraries), rep(c(1, -1, 1), each = floor(libraries/3)), rep(c(-1, 1, 1), each = floor(libraries/3))),
-          starts = Starts(nu = 10, omegaSquared = 0.01, sigmaSquared = c(1, 0.25, 0.25), tau = 1, theta = c(3, 0, 0))){
+          starts = Starts(nu = 10, omegaSquared = 0.01, sigmaSquared = c(1, 0.25, 0.25), tau = 0.1, theta = c(3, 0, 0))){
 
   stopifnot(libraries >= 3)
 
