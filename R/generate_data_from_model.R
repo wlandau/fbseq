82fafe102e8000c539d4d5277c3ce1888d103d03
wlandau <will.landau@gmail.com>
2015-11-19 15:16:53 -0600

    tweak

--- R/generate_data_from_model.R
+++ R/generate_data_from_model.R
@@ -24,38 +24,39 @@ generate_data_from_model = function(genes, design, truth, priors = "Laplace"){
   stopifnot(libraries >= 3)
   stopifnot(libraries == nrow(design))
 
-  truth@xi = numeric(0)
-  truth@beta = numeric(0)
-  for(l in 1:ncol(design)){
-    
-    if(priors == "Laplace"){
-      xi = rexp(genes, rate = truth@k[1])
-    } else if(priors == "t"){
-      xi = 1/rgamma(genes, shape = truth@q[1], rate = truth@r[1])
-    } else if(priors == "horseshoe"){
-      xi = abs(rcauchy(genes))
-    } else {
-      xi = rep(1, genes)
-    }
-    xi[xi > 10] = sample(xi[xi <= 10], sum(xi > 10), replace = T)
+  counts = NA
+  while(!all(is.finite(counts))){
+    truth@xi = numeric(0)
+    truth@beta = numeric(0)
+    for(l in 1:ncol(design)){
+      if(priors == "Laplace"){
+        xi = rexp(genes, rate = truth@k[1])
+      } else if(priors == "t"){
+        xi = 1/rgamma(genes, shape = truth@q[1], rate = truth@r[1])
+      } else if(priors == "horseshoe"){
+        xi = abs(rcauchy(genes))
+      } else {
+        xi = rep(1, genes)
+      }
+      xi[xi > 5] = sample(xi[xi <= 5], sum(xi > 5), replace = T)
 
-    beta = rtruncnorm(n = genes, mean = truth@theta[l], sd = sqrt(truth@sigmaSquared[l] * xi), a = -15, b = 15)
-    truth@xi = c(truth@xi, xi)
-    truth@beta = c(truth@beta, beta)
-  }  
+      beta = rnorm(n = genes, mean = truth@theta[l], sd = sqrt(truth@sigmaSquared[l] * xi))
+      truth@xi = c(truth@xi, xi)
+      truth@beta = c(truth@beta, beta)
+    }  
 
-  truth@gamma = 1/rgamma(genes, shape = truth@nu/2, rate = truth@nu*truth@tau/2)
+    truth@gamma = 1/rgamma(genes, shape = truth@nu/2, rate = truth@nu*truth@tau/2)
+    truth@rho = rnorm(libraries, 0, sqrt(truth@omegaSquared))
+    rhomat = matrix(rep(truth@rho, each = genes), ncol = libraries)
 
-  truth@rho = rnorm(libraries, 0, sqrt(truth@omegaSquared))
-  rhomat = matrix(rep(truth@rho, each = genes), ncol = libraries)
+    gammat = matrix(rep(truth@gamma, times = libraries), ncol = libraries)
+    epsilon = matrix(rnorm(libraries*genes, rhomat, sqrt(gammat)), ncol = libraries)
 
-  gammat = matrix(rep(truth@gamma, times = libraries), ncol = libraries)
-  epsilon = matrix(rnorm(libraries*genes, rhomat, sqrt(gammat)), ncol = libraries)
+    beta = matrix(truth@beta, nrow = genes)
+    lambda = t(design %*% t(beta))
 
-  beta = matrix(truth@beta, nrow = genes)
-  lambda = t(design %*% t(beta))
-
-  counts = matrix(rpois(genes*libraries, exp(lambda + epsilon)), nrow = genes)
+    suppressWarnings(counts <- matrix(rpois(genes*libraries, exp(lambda + epsilon)), nrow = genes))
+  }
   rownames(counts) = paste("gene_", 1:genes, sep="")
   colnames(counts) = rownames(design)
 
--- R/namespace.R
+++ R/namespace.R
@@ -2,5 +2,4 @@
 #' @import ggplot2
 #' @import methods
 #' @importFrom reshape2 melt
-#' @importFrom truncnorm rtruncnorm
 NULL
