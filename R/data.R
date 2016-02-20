5e415fbba18ea1d263060aeb5c99a55c4b5f675e
wlandau <will.landau@gmail.com>
2016-02-19 18:56:27 -0600

    Cite Paschold eta al. (2012) data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' @name paschold
 #' @title paschold
-#' @description a \code{Scenario} object guiding the analysis of the Paschold dataset.
+#' @description a \code{Scenario} object guiding the analysis of the Paschold et al (2012) dataset 
+#' (http://www.ncbi.nlm.nih.gov/pubmed/23086286).
 #' @docType data
-#' @author Paschold
+#' @author Paschold et al. (http://www.ncbi.nlm.nih.gov/pubmed/23086286)
 NULL
