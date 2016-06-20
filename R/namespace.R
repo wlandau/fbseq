44dcc61772bce016e5681710173d61e466d30cdc
wlandau <will.landau@gmail.com>
2016-06-20 17:00:57 -0400

    Add imports

--- R/namespace.R
+++ R/namespace.R
@@ -3,4 +3,6 @@
 #' @importFrom actuar qinvgamma
 #' @importFrom parallel mclapply
 #' @importFrom reshape2 melt
+#' @importFrom stats qnorm rexp rgamma rnorm rpois rt runif var
+#' @importFrom utils data
 NULL
